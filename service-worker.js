7c37c294b381d27fc7237cde1b5aa71eb1a2a898
evertvanlaar <evert.van.laar@gmail.com>
2026-03-27 16:26:31 +0100

    harde herstel commit

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.86'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v86';
+const CACHE_NAME = 'kalanera-cache-v1.0.88'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v1.0.88';
 
 const STATIC_ASSETS = [
   '/',
