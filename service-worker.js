c88db689d126f1f1fb7b0089e6654f26791f2497
evertvanlaar <evert.van.laar@gmail.com>
2026-03-27 14:00:40 +0100

    v nr omhoogst 1.0.85

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.83'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v83';
+const CACHE_NAME = 'kalanera-cache-v1.0.85'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v85';
 
 const STATIC_ASSETS = [
   '/',
