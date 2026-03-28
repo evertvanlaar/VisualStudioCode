78a26b5ca8f517176c7c696ef11ea1efde140827
evertvanlaar <evert.van.laar@gmail.com>
2026-03-28 12:50:11 +0100

    sw uupda

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.92'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v1.0.92';
+const CACHE_NAME = 'kalanera-cache-v1.0.93'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v1.0.93';
 
 const STATIC_ASSETS = [
   '/',
