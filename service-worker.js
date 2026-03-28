af1bdf6b1f41c2a95e47a9138d6d3fee535c56ae
evertvanlaar <evert.van.laar@gmail.com>
2026-03-28 12:28:27 +0100

    render gaat niet

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.91'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v1.0.91';
+const CACHE_NAME = 'kalanera-cache-v1.0.92'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v1.0.92';
 
 const STATIC_ASSETS = [
   '/',
