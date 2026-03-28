361dc9d9623f086a2afdf6b862dea6a00042d468
evertvanlaar <evert.van.laar@gmail.com>
2026-03-28 12:14:00 +0100

    versie omhoog

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.90'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v1.0.90';
+const CACHE_NAME = 'kalanera-cache-v1.0.91'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v1.0.91';
 
 const STATIC_ASSETS = [
   '/',
