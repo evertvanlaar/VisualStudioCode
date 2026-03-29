50b48c7974d4b2bb71b5801d4a507066b3616220
evertvanlaar <evert.van.laar@gmail.com>
2026-03-29 21:12:16 +0200

    totale restyiling buttons menu en alles optimaal. en werkend

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
-const CACHE_NAME = 'kalanera-cache-v1.0.100'; // Verhoog naar 83
-const IMAGE_CACHE = 'kalanera-images-v100';
+const CACHE_NAME = 'kalanera-cache-v1.0.101'; // Verhoog naar 83
+const IMAGE_CACHE = 'kalanera-images-v101';
 
 const STATIC_ASSETS = [
   '/',
