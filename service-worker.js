3425cb751c116e9510d8b8f85a7e3d484bf7a6dc
evertvanlaar <evert.van.laar@gmail.com>
2026-03-31 19:34:15 +0200

    version upd

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
-const VERSION = '1.0.8'; // Dit sturen we naar de Sheet
-const CACHE_NAME = 'kalanera-cache-v1.0.108'; // Dit dwingt de code-update af
-const IMAGE_CACHE = 'kalanera-images-v108'; // Dit laten we lekker staan voor de snelheid
+const VERSION = '1.0.9'; // Dit sturen we naar de Sheet
+const CACHE_NAME = 'kalanera-cache-v1.0.109'; // Dit dwingt de code-update af
+const IMAGE_CACHE = 'kalanera-images-v109'; // Dit laten we lekker staan voor de snelheid
 
 // VOEG DIT TOE: Luister naar vragen van de app
 self.addEventListener('message', (event) => {
