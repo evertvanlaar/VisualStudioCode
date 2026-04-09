6d261f56eb4821b3dc0b5939b071962bc5d0c606
evertvanlaar <evert.van.laar@gmail.com>
2026-04-09 20:28:53 +0200

    versio up 46

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const iconMap = {
 };
 
 // --- STAP 2: VERSIE-BEHEER (SLECHTS OP 1 PLEK AANPASSEN) ---
-const APP_VERSION = '1.0.45'; // <--- Pas VOORTAAN alleen nog maar dit getal aan!
+const APP_VERSION = '1.0.46'; // <--- Pas VOORTAAN alleen nog maar dit getal aan!
 let CURRENT_APP_VERSION = APP_VERSION; 
 
 if ('serviceWorker' in navigator) {
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
-const VERSION = '1.0.45'; // Dit sturen we naar de Sheet
-const CACHE_NAME = 'kalanera-cache-v1.0.145'; // Dit dwingt de code-update af
-const IMAGE_CACHE = 'kalanera-images-v145'; // Dit laten we lekker staan voor de snelheid
+const VERSION = '1.0.46'; // Dit sturen we naar de Sheet
+const CACHE_NAME = 'kalanera-cache-v1.0.146'; // Dit dwingt de code-update af
+const IMAGE_CACHE = 'kalanera-images-v146'; // Dit laten we lekker staan voor de snelheid
 
 // VOEG DIT TOE: Luister naar vragen van de app
 self.addEventListener('message', (event) => {
