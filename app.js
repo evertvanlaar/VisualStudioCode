a9939285173264af41743bda95d5b99dcad21895
evertvanlaar <evert.van.laar@gmail.com>
2026-04-17 17:02:46 +0200

    griekse versie toegevoegd en css opgeschoond

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const iconMap = {
 };
 
 // --- STAP 2: VERSIE-BEHEER (SLECHTS OP 1 PLEK AANPASSEN) ---
-const APP_VERSION = '1.0.55'; // <--- Pas VOORTAAN alleen nog maar dit getal aan!
+const APP_VERSION = '1.0.60'; // <--- Pas VOORTAAN alleen nog maar dit getal aan!
 let CURRENT_APP_VERSION = APP_VERSION; 
 
 if ('serviceWorker' in navigator) {
