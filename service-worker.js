87bd6aa5694861324f9680de27994af0eb3b0e85
evertvanlaar <evert.van.laar@gmail.com>
2026-04-26 19:52:48 +0200

    titl grieks kala nera netjes afgebroken

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
-const VERSION = '1.0.116'; // Dit sturen we naar de Sheet
-const CACHE_NAME = 'kalanera-cache-v1.0.116'; // Dit dwingt de code-update af
-const IMAGE_CACHE = 'kalanera-images-v1.0.116'; // Afbeeldingen apart cachen voor snelheid
+const VERSION = '1.0.117'; // Dit sturen we naar de Sheet
+const CACHE_NAME = 'kalanera-cache-v1.0.117'; // Dit dwingt de code-update af
+const IMAGE_CACHE = 'kalanera-images-v1.0.117'; // Afbeeldingen apart cachen voor snelheid
 
 // VOEG DIT TOE: Luister naar vragen van de app
 self.addEventListener('message', (event) => {
