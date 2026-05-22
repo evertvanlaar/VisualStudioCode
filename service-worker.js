58246765482b2d1619bdefa15ad708c2145bd421
evertvanlaar <evert.van.laar@gmail.com>
2026-05-22 10:24:06 +0200

    versie update totaal ivm cache fix service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
-const VERSION = '3.1.3'; // Dit sturen we naar de Sheet
-const CACHE_NAME = 'kalanera-cache-v3.1.3'; // Dit dwingt de code-update af
-const IMAGE_CACHE = 'kalanera-images-v3.1.3'; // Afbeeldingen apart cachen voor snelheid
+const VERSION = '3.1.4'; // Dit sturen we naar de Sheet
+const CACHE_NAME = 'kalanera-cache-v3.1.4'; // Dit dwingt de code-update af
+const IMAGE_CACHE = 'kalanera-images-v3.1.4'; // Afbeeldingen apart cachen voor snelheid
 
 // VOEG DIT TOE: Luister naar vragen van de app
 self.addEventListener('message', (event) => {
